559527024cdfddd19164b4d7c2e6d0a2594b6e99
Murilex7 <muriloharanha.com>
2026-05-19 10:38:23 -0300

    Usando Array

---
+++ DESAFIO/script.js
@@ -0,0 +1,9 @@
+let aluno = [
+    {aluno: "Breno", idade: 17, materiaFavorita: "Matemática"},
+    {aluno: "Luan", idade: 20, materiaFavorita: "Artes"},
+    {aluno: "Ryan", idade: 15, materiaFavorita: "Inglês"},
+    {aluno: "Vinicius", idade: 18, materiaFavorita: "Geografia"},
+    {aluno: "Wesley", idade: 10, materiaFavorita: "Português"}
+];
+alert(aluno[0].idade);
+alert(aluno[4].materiaFavorita);
